81e7ad97a3bd1e3e6994b8912dd719baa19c0072
Katie Stewart <stewarkr@miamioh.edu>
2014-03-09 21:25:01 +0000

    Working on the menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 DinnerDash::Application.routes.draw do
+  
   resources :categories
-
   resources :items
+  resources :menu, only: [:index]
 
   get 'about', to: 'marketing#about'
   get 'faqs', to: 'marketing#faqs'
